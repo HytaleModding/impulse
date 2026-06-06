0e90f4a0517a1258e6b2b709fe194dd01859caa3
Blovien <blovien@hytalemodding.dev>
2026-06-06 17:36:42 +0200

    fix(examples): arm explosives when impacts settle

--- impulse-examples/src/main/java/dev/hytalemodding/impulse/examples/systems/ExplosiveFuseTickSystem.java
+++ impulse-examples/src/main/java/dev/hytalemodding/impulse/examples/systems/ExplosiveFuseTickSystem.java
@@ -4,6 +4,7 @@ import com.hypixel.hytale.component.ArchetypeChunk;
 import com.hypixel.hytale.component.CommandBuffer;
 import com.hypixel.hytale.component.ComponentType;
 import com.hypixel.hytale.component.Ref;
+import com.hypixel.hytale.component.RemoveReason;
 import com.hypixel.hytale.component.Store;
 import com.hypixel.hytale.component.query.Query;
 import com.hypixel.hytale.component.system.QuerySystem;
@@ -84,7 +85,7 @@ public final class ExplosiveFuseTickSystem extends EntityTickingSystem<EntitySto
             spaceId,
             center,
             explosive);
-        commandBuffer.removeEntity(ref, com.hypixel.hytale.component.RemoveReason.REMOVE);
+        commandBuffer.removeEntity(ref, RemoveReason.REMOVE);
     }
 
     @Nonnull
