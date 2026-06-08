9262997d9bf5982923dcef7fb85834cef2d63ca6
Blovien <blovien@hytalemodding.dev>
2026-06-08 23:13:14 +0200

    fix(core): restore perf command registration
    Signed-off-by: Blovien <blovien@hytalemodding.dev>

--- impulse-core/src/main/java/dev/hytalemodding/impulse/core/internal/commands/ImpulseCommand.java
+++ impulse-core/src/main/java/dev/hytalemodding/impulse/core/internal/commands/ImpulseCommand.java
@@ -3,6 +3,7 @@ package dev.hytalemodding.impulse.core.internal.commands;
 import com.hypixel.hytale.server.core.command.system.basecommands.AbstractCommandCollection;
 import dev.hytalemodding.impulse.core.internal.commands.backend.BackendCommand;
 import dev.hytalemodding.impulse.core.internal.commands.debug.DebugCommand;
+import dev.hytalemodding.impulse.core.internal.commands.perf.PerfCommand;
 import dev.hytalemodding.impulse.core.internal.commands.settings.SettingsCommand;
 
 public class ImpulseCommand extends AbstractCommandCollection {
@@ -12,6 +13,7 @@ public class ImpulseCommand extends AbstractCommandCollection {
         addSubCommand(new BackendCommand());
         addSubCommand(new CleanCommand());
         addSubCommand(new DebugCommand());
+        addSubCommand(new PerfCommand());
         addSubCommand(new SettingsCommand());
         addSubCommand(new SpaceCommand());
     }
